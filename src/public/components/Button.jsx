7ec18859338ca270c454e42ee16d894571b6dce1
theerlant <ttrinanda2@gmail.com>
2026-03-12 15:01:56 +0800

    feat: responsive header

--- src/public/components/Button.jsx
+++ src/public/components/Button.jsx
@@ -1,8 +1,11 @@
 const baseClasses = "py-4 px-12 rounded-sm not-[disabled]:hover:cursor-pointer";
-const smallClasses = "py-2.5!";
+const smallClasses = "py-2.5! px-8! text-xs!";
 const variantClasses = {
   primary: "bg-[#DB4444] hover:bg-[#E07575] disabled:bg-[#E07575] text-white",
-  outlined: "bg-transparent border hover:border-black/50 hover:text-[#7D8184]",
+  outlined:
+    "bg-transparent outline -outline-offset-2 hover:outline-black/50 hover:text-[#7D8184]",
+  outlinedAlt:
+    "bg-transparent outline -outline-offset-2 outline-[#DB4444] text-[#DB4444]",
 };
 
 export const Button = ({
